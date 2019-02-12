ffbcc19298d3cf951a15a4d292ae43c69bac9e78
Hafizzle <34069775+Hafizzle@users.noreply.github.com>
2019-02-11 19:55:44 -0600

    Initial Commit
    Initial Commit

---
+++ Fizly/Models/Movie.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Fizly.Models
+{
+    public class Movie
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+
+    }
+
+
+}
