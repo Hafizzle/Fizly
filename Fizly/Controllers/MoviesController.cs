6a3bd1d2414bbef023c12c773e793d0610d5d025
Hafizzle <34069775+Hafizzle@users.noreply.github.com>
2019-02-21 20:35:36 -0600

    ViewModels added

--- Fizly/Controllers/MoviesController.cs
+++ Fizly/Controllers/MoviesController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using Fizly.Models;
+using Fizly.ViewModels
 
 namespace Fizly.Controllers
 {
@@ -13,38 +14,32 @@ namespace Fizly.Controllers
         
     {
 
-        [Route("movies/released/{year}/{month:regex(\\d{4}):range(1, 12)}")]
+        
         // GET: Movies/Random
         public ActionResult Random()
         {
             var movie = new Movie() { Name = "Shrek!" };
+            var customers = new List<Customer>
+            {
+                new Customer {Name = "Customer 1"},
+                new Customer {Name = "Customer 2"}
+            };
 
-            return View(movie);
+
+            var viewModel = new RandomMovieViewModel
+            {
+                Movie = movie,
+                Customers = customers
+            };
+
+            return View(viewModel);
         }
 
+        //[Route("movies/released/{year}/{month:regex(\\d{2}):range(1, 12)}")]
+        //Parameter set to only allow months (1-12)
 
-
-        public ActionResult Edit(int id)//Edit is action, id is id
-        {
-            return Content("id=" + id);
-        }
-
-        public ActionResult Index(int? pageIndex, string sortBy)
-        {
-            if (!pageIndex.HasValue)
-                pageIndex = 1;
-
-            if (String.IsNullOrWhiteSpace(sortBy))
-                sortBy = "Name";
-
-            return Content(String.Format("pageIndex={0}&sortBy={1}", pageIndex, sortBy));
-
-        }
-
-        public ActionResult ByReleaseDate(int year, int month)
-        {
-            return Content(year + "/" + month);
-        }
+        
+        
         
 
         }
